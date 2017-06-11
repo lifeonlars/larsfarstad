9b3a1f63881a2923c243786cac056730a327e2ce
Lars Farstad <lars.farstad@gmail.com>
2017-06-11 19:44:00 +1000

    Remove modernizer file. Remove animate.css dependency

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,7 @@ gulp.task('extras', () => {
   }).pipe(gulp.dest('docs'));
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'docs']));
+gulp.task('clean', del.bind(null, ['.tmp']));
 
 gulp.task('serve', () => {
   runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
